0f955d01d81613ad1b661fe14bf8783490ed0678
stasian <stanislav.tiryoshin@gmail.com>
2022-07-01 00:25:30 +0600

    Ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,19 @@ app.use(express.urlencoded({ extended: false }))
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/todos', require('./routes/todoRoutes'))
 
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
+}
+
 app.use(errorHandler)
 
 app.listen(port, () => console.log(`Port ${port} is up and running baby`))
